36360bb7b523d94d09db2f4fb859a1e053d4356b
duarteeuler <euler21duarte@gmail.com>
2023-01-26 10:59:22 +0000

    add func addBookToLibrary and Clear Method values

--- index.js
+++ index.js
@@ -2,21 +2,40 @@ let myLibrary = [];
 let arrayId = 0;
 
 function addBookToLibrary(){
-  
+  let bookTitle = document.getElementById('title');
+  let bookAuthor = document.getElementById('author');
+  let status = document.getElementById('status');
+
+  // object
+  myLibrary[arrayId] = {
+    title: bookTitle.value,
+    author: bookAuthor.value,
+    status: status.value
+  };
+
+  console.log(myLibrary);
+  arrayId+=1; 
+
+  // Clear Method
+  document.getElementById('title').value = "";
+  document.getElementById('author').value = "";
+  document.getElementById('status').value = "read";
 
 }
+
+
 // the constructor
-function Book(name, author, pages, situation) {
+function Book(name, author) {
   this.name = name // Referencia ao obj atual
   this.author = author
-  this.pages = pages
-  this.situation = situation
+  
   this.info = function() {
-    console.log(name+author+pages+situation);
+    console.log(name+author);
   }
 }
 
-const threeBody = new Book('The Three Body Problem', 'Xines', '300 pages', 'not ready yet')
+const threeBody = new Book('The Three Body Problem', 'Xines')
 threeBody.info()
+
 
 
